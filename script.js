71c8151374b59e6fdc6535b7ff6f51f0bb1ab456
Ebru Özdemir <ozdemir.ebbru@gmail.com>
2023-01-09 17:36:24 +0300

    new features added

--- script.js
+++ script.js
@@ -1,94 +1,67 @@
-window.addEventListener("DOMContentLoaded", createMainButtons());
+let data = null;
 
+window.addEventListener("DOMContentLoaded", createMainButtons());
 function createMainButtons() {
   fetch("data.json")
-    .then(function (response) {
-      return response;
-    })
     .then(function (data) {
       return data.json();
     })
     .then(function (obj) {
-      const main = obj.main;
-      const buttons = main.map(
-        (data) =>
-          `<li class="${data.buttonClassList}" >
-            <a href="${data.buttonUrl}" target="${data.urlTarget}" style="${data.buttonStyle}" class="${data.anchorClassList}" id="${data.buttonId}" onclick="createInnerButtons(event)">
-            ${data.buttonTextContent}
-            </a>
-            </li>`
-      );
-      document.querySelector(".lists").innerHTML = buttons.join(""); //lilili
+      data = obj;
+      setHeader(obj.mainPage.header);
+      setButtons(obj.mainPage.buttons);
     })
     .catch(function (error) {
       console.log("Fetch problem show: " + error.message);
     });
 }
-
 function createInnerButtons(event) {
   event.preventDefault();
-  fetch("data.json")
-    .then(function (response) {
-      return response;
-    })
-    .then(function (data) {
-      return data.json();
-    })
-    .then(function (obj) {
-      const id = event.target.id;
-      switch (id) {
-        case "islem":
-          back = `<a href="index.html" class="${obj.anchor.buttonClassList}">Anasayfa</a>`;
-          const islemKaydi = obj.islemKaydı;
-          buttons = islemKaydi.map(
-            (data) =>
-              `<li class="${data.buttonClassList}" >
-              <a href="${data.buttonUrl}" target="${data.urlTarget}" style="${data.buttonStyle}" class="${data.anchorClassList}" id="${data.buttonId}" onclick="createInnerButtons(event)">
-              ${data.buttonTextContent}</a></li>`
-          );
-          break;
-        case "durum":
-          back =`<a href="index.html" class="${obj.anchor.buttonClassList}" >Anasayfa</a>`;
-          const durumTespit = obj.durumTespit;
-          buttons = durumTespit.map(
-            (data) =>
-              `<li class="${data.buttonClassList}" >
-                <a href="${data.buttonUrl}" target="${data.urlTarget}" style="${data.buttonStyle}" class="${data.anchorClassList}" id="${data.buttonId}" onclick="createInnerButtons(event)">
-                ${data.buttonTextContent}</a></li>`
-          );
-          break;
-        case "otobüs":
-          const otobüs = obj.otobüs;
-          buttons = otobüs.map(
-            (data) =>
-              `<li class="${data.buttonClassList}" >
-                  <a href="${data.buttonUrl}" target="${data.urlTarget}" style="${data.buttonStyle}" class="${data.anchorClassList}" id="${data.buttonId}">
-                  ${data.buttonTextContent}</a></li>`
-          );
-          break;
-        case "durak":
-          const durak = obj.durak;
-          buttons = durak.map(
-            (data) =>
-              `<li class="${data.buttonClassList}" >
-                    <a href="${data.buttonUrl}" target="${data.urlTarget}" style="${data.buttonStyle}" class="${data.anchorClassList}" id="${data.buttonId}">
-                    ${data.buttonTextContent}</a></li>`
-          );
-          break;
-        case "aktarmaNoktası":
-          const aktarmaNoktası = obj.aktarmaNoktası;
-          buttons = aktarmaNoktası.map(
-            (data) =>
-              `<li class="${data.buttonClassList}" >
-                      <a href="${data.buttonUrl}" target="${data.urlTarget}" style="${data.buttonStyle}" class="${data.anchorClassList}" id="${data.buttonId}">
-                      ${data.buttonTextContent}</a></li>`
-          );
-          break;
-      }
-      document.querySelector(".back").innerHTML = back;
-      document.querySelector(".lists").innerHTML = buttons.join(""); //lilili
-    })
-    .catch(function (error) {
-      console.log("Fetch problem show: " + error.message);
-    });
+  obj = data;
+  const id = event.target.id;
+  switch (id) {
+    case "islem":
+      back = `<a href="index.html" class="${obj.anchor.buttonClassList}">Anasayfa</a>`;
+      setHeader(obj.islemKaydiPage.header);
+      setButtons(obj.islemKaydiPage.buttons);
+      break;
+    case "durum":
+      back = `<a href="index.html" class="${obj.anchor.buttonClassList}" >Anasayfa</a>`;
+      setHeader(obj.durumTespitPage.header);
+      setButtons(obj.durumTespitPage.buttons);
+      break;
+    case "otobüs":
+      setHeader(obj.otobusPage.header);
+      setButtons(obj.otobusPage.buttons);
+      break;
+    case "durak":
+      setHeader(obj.durakPage.header);
+      setButtons(obj.durakPage.buttons);
+      break;
+    case "aktarma":
+      setHeader(obj.aktarmaPage.header);
+      setButtons(obj.aktarmaPage.buttons);
+      break;
+  }
+  document.querySelector(".back").innerHTML = back;
+}
+function setHeader(obj) {
+  const newHeader = `<div class="${obj.divClassList}">
+  <h1 class="${obj.headerClassList}" style="${obj.headerStyle}">
+  ${obj.headerName}
+  </h1>
+  </div>`;
+  document.querySelector(".header").innerHTML = newHeader;
+}
+function setButtons(obj) {
+  const newButtons = obj.map(
+    (button) =>
+      `<li class="${button.buttonClassList}" >
+      <a href="${button.buttonUrl}" target="${button.urlTarget}"
+      style="${button.buttonStyle}" class="${button.anchorClassList}" 
+      id="${button.buttonId}" 
+      ${button.buttonUrl === "#" ? 'onclick="createInnerButtons(event)"' : ""}>
+      ${button.buttonName}</a></li>`
+  );
+  document.querySelector(".lists").innerHTML = newButtons.join("");
 }
